68aadfc5656b1e16dddc7ff3dce3ec1168721e99
Harsha Vibhute <vibhuteha1598@gmail.com>
2022-08-19 08:35:20 +0530

    Added Project to github

---
+++ DjangoMedicalApp/admin.py
@@ -0,0 +1,17 @@
+import imp
+import django
+from django.contrib import admin
+from DjangoMedicalApp.models import Company,Medicine,MedicalDetails,Employee,Customer,Bill,EmployeeSalary,BillDetails,CustomerRequest,CompanyAccount,CompanyBank,EmployeeBank
+# Register your models here.
+admin.site.register(Company)
+admin.site.register(Medicine)
+admin.site.register(MedicalDetails)
+admin.site.register(Employee)
+admin.site.register(Customer)
+admin.site.register(Bill)
+admin.site.register(EmployeeSalary)
+admin.site.register(BillDetails)
+admin.site.register(CustomerRequest)
+admin.site.register(CompanyAccount)
+admin.site.register(CompanyBank)
+admin.site.register(EmployeeBank)
